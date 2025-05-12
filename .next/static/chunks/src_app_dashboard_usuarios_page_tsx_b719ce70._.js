28910081ffab3065e6bcff3c6d827fde7c7d8807
israelpromarcas <goes95@gmail.com>
2025-05-12 02:51:13 -0400

    Add .next/static/chunks/src_app_dashboard_usuarios_page_tsx_b719ce70._.js 1747032672699

---
+++ .next/static/chunks/src_app_dashboard_usuarios_page_tsx_b719ce70._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_cd7df154._.js",
+  "static/chunks/src_e3f3298e._.js"
+],
+    source: "dynamic"
+});
