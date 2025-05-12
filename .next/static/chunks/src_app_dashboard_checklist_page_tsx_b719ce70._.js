67b96202a801b11897c2054f0cde057633e46867
israelpromarcas <goes95@gmail.com>
2025-05-12 02:51:15 -0400

    Add .next/static/chunks/src_app_dashboard_checklist_page_tsx_b719ce70._.js 1747032675064

---
+++ .next/static/chunks/src_app_dashboard_checklist_page_tsx_b719ce70._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_074db698._.js",
+  "static/chunks/node_modules_2a573733._.js"
+],
+    source: "dynamic"
+});
